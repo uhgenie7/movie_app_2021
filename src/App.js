286586bfb96e4e25d427c0ebe8b12c46b0bba119
uhj1993 <dzzd777@naver.com>
2021-02-11 15:36:08 +0900

    JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import Potato from './Potato';
+import React from 'react';  
+function Food({ fav}) {
+  return <div>I love { fav}</div>
+}
 
 function App() {
   return (
     <div>
-      hello!
-       <Potato/>
+      <h1>hello!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="라면"/>
+      <Food fav="만두"/>
+      <Food fav="스파게티"/>
+       
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato() {
-    return <h3>hello there</h3>
-}
-
-export default Potato;

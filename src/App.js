865c34c0065c69cd36308d59733204a4e64b8d8c
uhj1993 <dzzd777@naver.com>
2021-02-12 21:49:54 +0900

    기능추가:영화  api를 불러들여 기능을 추가함. axios 설치 및 적용.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';  
-function Food({ fav}) {
-  return <div>I love { fav}</div>
-}
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div>
-      <h1>hello!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="라면"/>
-      <Food fav="만두"/>
-      <Food fav="스파게티"/>
-       
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
+  }
 }
 
 export default App;
